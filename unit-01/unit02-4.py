31de23990c24ec22bf31dec0166035f1da2e3d53
ampeplinski <alex.peplinski@student.cuaa.edu>
2024-04-05 21:32:20 -0400

    strings

---
+++ unit-01/unit02-4.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    
+    years_old = int(input("How old are you?: "))
+    first_name = str(input("what is your first name?:"))
+    last_name = str(input("what is your last name?:"))
+
+    print(f"hello{first_name}! do i spell your last name like this?: {last_name}. also I remember your age, it is {years_old}")
+    print("hello"+ first_name +"! do i spell your last name like this?: "+ last_name + ". also I remember your age, it is" + str(years_old))
+print("Hello World")
+
+if __name__ == "__main__":
+    main()
