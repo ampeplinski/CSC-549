61325ff9eae7674f868b871c10d5ac390f250cc1
ampeplinski <alex.peplinski@student.cuaa.edu>
2024-04-07 18:04:45 -0400

    partd

---
+++ unit-01/unit03-03d.py
@@ -0,0 +1,18 @@
+
+def areOfACircle(r):
+    pie = 3.14
+
+    area_num = pie * (r ** 2)
+
+    return area_num
+
+
+def main():
+    result = areOfACircle(3)
+    print(result)
+
+
+print("Hello World")
+
+if __name__ == "__main__":
+    main()
